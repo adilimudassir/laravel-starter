03035e2aab2dda4c55932ef0f147f86276017284
Mudassir Ahmad Adili <mudassiradili4u@gmail.com>
2020-05-29 16:29:10 +0100

    update routes

--- resources/views/components/frontend/partials/navbar.blade.php
+++ resources/views/components/frontend/partials/navbar.blade.php
@@ -19,11 +19,11 @@
                 <!-- Authentication Links -->
                 @guest
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    <a class="nav-link" href="{{ route('frontend.auth.login') }}">{{ __('Login') }}</a>
                 </li>
                 @if (Route::has('register'))
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    <a class="nav-link" href="{{ route('frontend.auth.register') }}">{{ __('Register') }}</a>
                 </li>
                 @endif
                 @else
@@ -34,12 +34,12 @@
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                        <a class="dropdown-item" href="{{ route('frontend.auth.logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
                         </a>
 
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        <form id="logout-form" action="{{ route('frontend.auth.logout') }}" method="POST" style="display: none;">
                             @csrf
                         </form>
                     </div>
