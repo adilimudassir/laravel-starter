b53bd5550b37da240949e7ed0a79b32f262511c9
Mudassir Ahmad Adili <mudassiradili4u@gmail.com>
2020-06-04 10:23:46 +0100

    add assets

---
+++ public/js/backend.js
@@ -0,0 +1,27 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["/js/backend"],{
+
+/***/ "./resources/js/backend/app.js":
+/*!*************************************!*\
+  !*** ./resources/js/backend/app.js ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+
+/***/ 1:
+/*!*******************************************!*\
+  !*** multi ./resources/js/backend/app.js ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /home/adilimudassir/my-projects/laravel-starter/resources/js/backend/app.js */"./resources/js/backend/app.js");
+
+
+/***/ })
+
+},[[1,"/js/manifest"]]]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIvanMvYmFja2VuZC5qcyIsInNvdXJjZVJvb3QiOiIifQ==
