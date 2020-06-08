44e6bee7de38310ac56787ef8b5d3fc1651ca88c
Mudassir Ahmad Adili <mudassiradili4u@gmail.com>
2020-06-08 07:34:08 +0100

    update layouts

--- resources/views/components/backend/layouts/app.blade.php
+++ resources/views/components/backend/layouts/app.blade.php
@@ -1,8 +1,20 @@
 <x-layouts.app :title="$title">
+    <x-slot name="styles">
+        {{ $before_styles ?? null }}
+        <link href="{{ mix('css/backend.css') }}" rel="stylesheet">
+        {{ $after_styles ?? null }}
+    </x-slot>
     <x-backend.partials.navbar />
     {{ Breadcrumbs::render() }}
-    <main class="py-4">
+    <main class="">
         {{ $slot }}
     </main>    
     <x-backend.partials.footer />
+    <x-slot name="scripts">
+        {{ $before_scripts ?? null }}
+        <script src="{{ mix('js/manifest.js') }}"></script>
+        <script src="{{ mix('js/vendor.js') }}"></script>
+        <script src="{{ mix('js/backend.js') }}"></script>
+        {{ $after_scripts ?? null }}
+    </x-slot>
 </x-layouts.app>
