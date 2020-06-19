0bc00d3bee67996b0ed902eff75256ec16d306dc
Mudassir Ahmad Adili <mudassiradili4u@gmail.com>
2020-06-19 17:03:37 +0100

    add card component

---
+++ resources/views/components/backend/partials/card.blade.php
@@ -0,0 +1,23 @@
+<div class="card">
+    @if (isset($header))
+        <div class="card-header">
+            {{ $header }}
+
+            @if (isset($headerActions))
+                <div class="card-header-actions">
+                    {{ $headerActions }}
+                </div>
+            @endif
+        </div>
+    @endif
+    @if (isset($body))
+        <div class="card-body">
+            {{ $body }}
+        </div>
+    @endif
+    @if (isset($footer))
+        <div class="card-footer">
+            {{ $footer }}
+        </div>
+    @endif
+</div>
