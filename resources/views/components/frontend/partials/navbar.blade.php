6189100bb8b7a1e04ba44268494e883085747dec
Mudassir Ahmad Adili <mudassiradili4u@gmail.com>
2020-07-04 22:28:21 +0100

    wip

--- resources/views/components/frontend/partials/navbar.blade.php
+++ resources/views/components/frontend/partials/navbar.blade.php
@@ -3,8 +3,7 @@
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'Laravel') }}
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -21,32 +20,35 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('frontend.auth.login') }}">Login</a>
                 </li>
-                    @if(config('access.enable_registration'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('frontend.auth.register') }}">Register</a>
-                        </li>
-                    @endif
+                @if(config('access.enable_registration'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('frontend.auth.register') }}">Register</a>
+                </li>
+                @endif
                 @else
-                    @can('access-backend')
-                    <a href="{{ route('backend.dashboard') }}" class="btn btn-outline-success">Admin Panel</a>
-                    @endcan
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
+                @can('access-backend')
+                <a href="{{ route('backend.dashboard') }}" class="btn btn-outline-success">Admin Panel</a>
+                @endcan
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('frontend.profile') }}">
+                            Profile
                         </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('frontend.auth.logout') }}" onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('frontend.auth.logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
+                        <a 
+                            class="dropdown-item" 
+                            href="{{ route('frontend.auth.logout') }}" 
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                        <form id="logout-form" action="{{ route('frontend.auth.logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
                 @endguest
             </ul>
         </div>
